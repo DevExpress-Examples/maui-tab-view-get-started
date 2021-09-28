ff9262fc66fef9c6f2a30eb1e7f2288a8b89456e
Alexey Zhukov <alexey.zhukov@devexpress.com>
2021-09-28 12:12:57 +0300

    DevExpress Tab View for .NET MAUI Preview 8

---
+++ CS/GenerateTabItemsFromDataSource/TabView_GenerateItems/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Maui.Navigation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace TabView_GenerateItems
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				})
+				.ConfigureMauiHandlers((handlers => handlers.AddHandler<TabView, TabViewHandler>()));
+
+			return builder.Build();
+		}
+	}
+}
