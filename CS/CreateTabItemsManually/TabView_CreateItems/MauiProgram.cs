1b9e30be258b45d408ee54f8b65f14703c3a0620
Alexey Zhukov <alexey.zhukov@devexpress.com>
2021-09-29 10:14:32 +0300

    Clean code for readme

--- CS/CreateTabItemsManually/TabView_CreateItems/MauiProgram.cs
+++ CS/CreateTabItemsManually/TabView_CreateItems/MauiProgram.cs
@@ -1,25 +1,20 @@
-﻿using DevExpress.Maui.Navigation;
-using Microsoft.Maui;
+﻿using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Controls.Compatibility;
 using Microsoft.Maui.Controls.Hosting;
+using DevExpress.Maui.Navigation;
 
-namespace TabView_CreateItems
-{
-	public static class MauiProgram
-	{
-		public static MauiApp CreateMauiApp()
-		{
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				})
-				.ConfigureMauiHandlers((handlers => handlers.AddHandler<TabView, TabViewHandler>()));
-
-			return builder.Build();
-		}
-	}
+namespace TabView_CreateItems {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .ConfigureMauiHandlers((handlers => handlers.AddHandler<TabView, TabViewHandler>()));
+            return builder.Build();
+        }
+    }
 }
