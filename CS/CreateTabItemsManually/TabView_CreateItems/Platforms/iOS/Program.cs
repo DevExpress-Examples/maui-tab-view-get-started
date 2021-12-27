7ab2366fe5bee8c3937a597b422bc93c7478afb7
Alexey Zhukov <alexey.zhukov@devexpress.com>
2021-12-27 16:26:29 +0300

    DevExpress Tab View for .NET MAUI Preview 11

---
+++ CS/CreateTabItemsManually/TabView_CreateItems/Platforms/iOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using ObjCRuntime;
+using UIKit;
+
+namespace TabView_CreateItems;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		UIApplication.Main(args, null, typeof(AppDelegate));
+	}
+}
---
+++ CS/GenerateTabItemsFromDataSource/TabView_GenerateItems/BoolToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace TabView_GenerateItems
+{
+    public class BoolToColorConverter:IValueConverter {
+        public Color FalseSource { get; set; }
+        public Color TrueSource { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (!(value is bool)) {
+                return null;
+            }
+            return (bool)value ? TrueSource : FalseSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
+
